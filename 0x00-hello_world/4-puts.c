faf40d5d64922d5ec3b28a346ad81afa7f253409
Barreyobar-o <akhatsokhebarret2@gmail.com>
2022-09-08 22:11:51 +0000

    C Programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * * main - where the program starts from
+ * * Return: return value 0
+ * */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
