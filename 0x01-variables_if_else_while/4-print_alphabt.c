b66f501d00348b9d87b8b47e6a0c85624a870c74
debarret <akhatsokhebarret2@gmail.com>
2022-09-10 08:04:49 -0700

    C Programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+
+/**
+* main - loops alphabet in lower case except 'q' & 'e'
+* Return: returns 0
+*/
+int main(void)
+{
+
+int ch
+
+
+
+for (ch = 'a'; ch <= 'z'; ch++)
+
+if (ch != 'e' && ch != 'q')
+putchar(ch);
+
+putchar('\n');
+
+return (0);
+
+}
