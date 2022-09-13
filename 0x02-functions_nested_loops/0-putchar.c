5f9e1188717f608eb5d2f8365c55dc82ce84d064
debarret <akhatsokhebarret2@gmail.com>
2022-09-13 15:17:31 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,29 +3,34 @@
 
 
 /**
-* main - print putchar
-* Return: always 0
+* main - prints the string _putchar with headers
+* Return: returns 0
 */
-
 
 int main(void)
 
 {
 
 _putchar('_');
+
 _putchar('p');
+
 _putchar('u');
+
 _putchar('t');
+
 _putchar('c');
+
 _putchar('h');
+
 _putchar('a');
+
 _putchar('r');
+
 _putchar('\n');
 
 
 
 return (0);
-
-
 
 }
